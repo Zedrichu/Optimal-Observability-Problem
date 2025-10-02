2748d5d863817b2ff563c69d406a120757547598
arthurbosquetti <arthur.bosquetti@gmail.com>
2025-10-03 00:35:46 +0200

    Added script to output statistics on runtime variance

---
+++ dynamic_solvers/runtime_variance_results.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import glob
+
+
+def compute_variance_results(file_pattern: str):
+    files = glob.glob(file_pattern)
+    if not files:
+        print(f"No files found matching pattern: {file_pattern}")
+        return
+    print(f"Found {len(files)} files matching pattern.")
+    dfs = [pd.read_csv(f) for f in files]
+    num_instances = len(dfs[0])
+    stats = []
+    for idx in range(num_instances):
+        times = np.array([np.float64(df.iloc[idx]['Time (s)']) for df in dfs], dtype=np.float64)
+        stats.append({
+            'Instance': f"{dfs[0].iloc[idx]['Variant']}-{dfs[0].iloc[idx]['Model']}",
+            'Time_mean': np.nanmean(times),
+            'Time_var': np.nanvar(times),
+        })
+    stats_df = pd.DataFrame(stats)
+    print(stats_df)
+
+if '__main__' == __name__:
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python dynamic_solvers/runtime_variance_results.py '<file_pattern>'")
+        print("Note: This assumes all result files contain the benchmark for the same list of OOP instances.")
+        sys.exit(1)
+
+    compute_variance_results(sys.argv[1])
