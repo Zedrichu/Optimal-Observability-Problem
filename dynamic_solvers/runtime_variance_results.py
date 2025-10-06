9450d6d3c6719b8182e5fc3855e35f29037bfb50
arthurbosquetti <arthur.bosquetti@gmail.com>
2025-10-06 12:51:59 +0200

    Updated script for runtime variance and cleanup old log files

--- dynamic_solvers/runtime_variance_results.py
+++ dynamic_solvers/runtime_variance_results.py
@@ -3,12 +3,13 @@ import numpy as np
 import glob
 
 def parse_time(time_str: str) -> float:
-    if time_str == 't.o.':
-        return 180.0
+    TIMEOUT = 180.0
+    if time_str == 't.o.' or float(time_str) > TIMEOUT*0.975:
+        return -1.0
     else:
         return float(time_str)
 
-def compute_variance_results(file_pattern: str):
+def output_variance_results(file_pattern: str):
     files = glob.glob(file_pattern)
     if not files:
         print(f"No files found matching pattern: {file_pattern}")
@@ -19,10 +20,21 @@ def compute_variance_results(file_pattern: str):
     stats = []
     for idx in range(num_instances):
         times = np.array([np.float64(parse_time(df.iloc[idx]['Time (s)'])) for df in dfs], dtype=np.float64)
+        statuses = np.array([df.iloc[idx]['Status'] for df in dfs], dtype=np.str_)
+        timeout_mask = (times == -1.0)
+        timeout_count = np.count_nonzero(timeout_mask)
+        unknown_mask = (statuses == 'UNKNOWN')
+        unknown_count = np.count_nonzero(unknown_mask)
+        # Filter out UNKNOWNs for stats
+        valid_times = times[~unknown_mask]
+        variance = np.var(valid_times) if valid_times.size > 0 else np.nan
+        mean = np.nanmean(valid_times) if valid_times.size > 0 else np.nan
         stats.append({
             'Instance': f"{dfs[0].iloc[idx]['Variant']}-{dfs[0].iloc[idx]['Model']}",
-            'Time_mean': np.nanmean(times),
-            'Time_var': np.nanvar(times),
+            'Mean Solve Time': mean,
+            'Var. Solve Time': variance,
+            '# Timeouts': timeout_count,
+            '# UNKNOWN': unknown_count
         })
     stats_df = pd.DataFrame(stats)
     print(stats_df)
@@ -35,4 +47,4 @@ if '__main__' == __name__:
         print("Note: This assumes all result files contain the benchmark for the same list of OOP instances.")
         sys.exit(1)
 
-    compute_variance_results(sys.argv[1])
+    output_variance_results(sys.argv[1])
