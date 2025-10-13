70941a5ce9dc01f625c1cd42dbf76e7071482ff0
arthurbosquetti <arthur.bosquetti@gmail.com>
2025-10-13 16:04:29 +0200

    Implement reordering of constraints in POPSPec and SSPSpec
    # Conflicts:
    #	dynamic_solvers/builders/pop/POPSpec.py

--- dynamic_solvers/builders/pop/POPSpec.py
+++ dynamic_solvers/builders/pop/POPSpec.py
@@ -108,15 +108,19 @@ class POPSpec(OOPSpec, ABC):
     def collect_constraints(self, threshold: str) -> List[z3.BoolRef]:
         self.console.print("\n  🛠️  Building constraints...", justify="center")
 
-        # t = Real('t', self.ctx)
-        constraint_builders = [
-            self.build_fully_observable_constraints(),
-            self.build_bellman_equations(),
-            [self.build_threshold_constraint(threshold)],
-            self.build_strategy_constraints(),
-            # [t <= 1, t >= 0],
-            self.build_observation_constraints(),
+        builders = [
+            lambda: [*self.build_fully_observable_constraints(), self.build_threshold_constraint(threshold)],
+            lambda: self.build_bellman_equations(),
+            lambda: [*self.build_strategy_constraints(), *self.build_observation_constraints()],
+            lambda: [],
         ]
+
+        self.console.print("\nApplying order of constraints:")
+        self.console.print(",".join(f"{i} <- {order}" for (i, order) in enumerate(self.order_constraints)))
+
+        constraint_builders = [builders[i]() for i in self.order_constraints]
+        # self.console.print("\nOrder of constraints after applying reordering:")
+        # self.console.print(constraint_builders)
 
         # Flatten constraints for solver loading
         all_constraints = list(chain.from_iterable(constraint_builders))
