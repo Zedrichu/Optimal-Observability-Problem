660a8cdb365cf725abd1a8df23542408594ac92e
Arthur Bosquetti <arthur.bosquetti@gmail.com>
2025-12-03 15:28:16 +0100

    Revert "Fixed bug with Grid dist. computation" (#23)
    * Revert "Fixed bug with Grid dist. computation (#22)"
    
    This reverts commit 4522d0dcc7ce62669109355ff994d71e55dc7279.
    
    * Cleaner fix

--- dynamic_solvers/builders/worlds.py
+++ dynamic_solvers/builders/worlds.py
@@ -77,8 +77,10 @@ class Grid(World):
         # Grid-specific bounds calculation
         goal_column = target % self.width
         column = source % self.width
-        y_delta = 1 if (column - goal_column) * (target - source) > 0 else 0
-        return abs(goal_column - column) + (abs(target - source) // self.width + y_delta)
+        goal_row = target // self.width
+        row = source // self.width
+
+        return abs(goal_column - column) + abs(goal_row - row)
 
 
 class Maze(World):
