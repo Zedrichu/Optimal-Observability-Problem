e6d17df21a88c66ab2517ba4c19076514194020b
arthurbosquetti <arthur.bosquetti@gmail.com>
2025-12-14 13:56:20 +0100

    Changed function declaration to take entire obs function

--- dynamic_solvers/builders/POMDPSpec.py
+++ dynamic_solvers/builders/POMDPSpec.py
@@ -172,17 +172,20 @@ class POMDPAdapter:
                                  "\n ____________________________________________________________________")
         return bellman_equations
 
-    def ssp_strategy_constraints_state_on(self, state_on: int) -> list[BoolRef]:
+    def infer_ssp_strategy_constraints(self, obs_function: list[int]) -> list[BoolRef]:
         strategy_constraints = []
-        for direction in self._spec.clusters:
-            if state_on in self._spec.clusters[direction]:
-                idx = state_on if state_on < self._spec.goal else state_on - 1
-                for a, action in enumerate(self._spec.actions):
-                    if action not in direction.actions:
-                        strategy_constraints.append(self._spec.X[idx][a] == (False if self._spec.determinism else 0))
-                    elif {action} == direction.actions:
-                        strategy_constraints.append(self._spec.X[idx][a] == (True if self._spec.determinism else 1))
-                break
+        for state, sensor_on in enumerate(obs_function):
+            if sensor_on != 1:
+                continue
+            for direction in self._spec.clusters:
+                if state in self._spec.clusters[direction]:
+                    idx = state if state < self._spec.goal else state - 1
+                    for a, action in enumerate(self._spec.actions):
+                        if action not in direction.actions:
+                            strategy_constraints.append(self._spec.X[idx][a] == (False if self._spec.determinism else 0))
+                        elif {action} == direction.actions:
+                            strategy_constraints.append(self._spec.X[idx][a] == (True if self._spec.determinism else 1))
+                    break
         return strategy_constraints
 
     # Delegate attribute access to wrapped spec for convenience
